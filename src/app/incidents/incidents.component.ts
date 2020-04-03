0dce4349dda436ca1012ccc80d12136bc19c7263
vpsmolina <verasmolinap@mail.ru>
2020-04-03 22:25:05 +0300

    filter

--- src/app/incidents/incidents.component.ts
+++ src/app/incidents/incidents.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component, Inject, Input, OnInit, Output } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { Router } from "@angular/router";
 import { select, Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { combineLatest, Observable, of } from "rxjs";
+import { map, startWith } from "rxjs/operators";
 import { IncidentsList } from "../data/incidents-list";
 import { Incident } from "../models/incident";
 import { IncidentData } from "../models/incident-data";
@@ -19,8 +21,11 @@ import { IncidentEvents } from "./incidentevents";
 })
 export class IncidentsComponent implements OnInit {
   public incident: Incident;
-  public incidents: Incident[] = IncidentsList;
+  /*public incidents: Incident[] = IncidentsList;*/
   public incidents$: Observable<Incident[]> = this._store.pipe(select(selectIncidentList));
+  filteredStates$: Observable<Incident[]> = this._store.pipe(select(selectIncidentList));
+  filter: FormControl;
+  filter$: Observable<string>;
   public action: IncidentEvents;
   public type = true;
   public index: number;
@@ -29,10 +34,14 @@ export class IncidentsComponent implements OnInit {
   public selectedIncidents: string;
   constructor(@Inject(TableService) private dataService: IncidentData,
               private _router: Router,
-              private _store: Store<AppState>) {}
-  public applySearch(search: string): void {
-    search === "" ? this.selectedIncidents = null :  this.selectedIncidents = search;
+              private _store: Store<AppState>) {
+    this.filter = new FormControl("");
+    this.filter$ = this.filter.valueChanges.pipe(startWith(""));
+    this.filteredStates$ = combineLatest(this.incidents$, this.filter$).pipe(
+      map(([incidents, filterString]) => (filterString === "") ? incidents : incidents.filter(incident => incident.name.toLowerCase().indexOf(filterString.toLowerCase()) !== -1)),
+    );
   }
+
   public addIncident(): void {
     this._router.navigate([`main/events/add`]);
   }
@@ -42,6 +51,7 @@ export class IncidentsComponent implements OnInit {
 
   ngOnInit(): void {
     this._store.dispatch(new GetIncidents());
+
   }
 
 }
