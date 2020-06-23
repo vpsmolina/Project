85a0c53847d86f5fa3c3ed0a39a19823ede92caf
vpsmolina <verasmolinap@mail.ru>
2020-06-23 12:10:38 +0300

    upg style

--- src/app/common/common.module.ts
+++ src/app/common/common.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from "@angular/core";
 import { AreaFilterPipe } from "./area-filter.pipe";
-import { HoverDirective } from "./hover.directive";
 import { StatusFilterPipe } from "./status-filter.pipe";
 
 @NgModule({
   declarations: [
-    HoverDirective,
     StatusFilterPipe,
     AreaFilterPipe,
   ],
   exports: [
-    HoverDirective,
     StatusFilterPipe,
     AreaFilterPipe,
   ],
